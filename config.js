5dd56bf4556f8ab841fbdb16b56be80a5287bce7
SimFischer <simon@familiefischer.de>
2026-09-14 11:49:06 +0200

    Supabase-Konfiguration eintragen

--- config.js
+++ config.js
@@ -4,8 +4,7 @@
   gehört niemals in dieses Repository.
 */
 window.APP_CONFIG = {
-  SUPABASE_URL: '',
-  SUPABASE_ANON_KEY: '',
+  SUPABASE_URL: 'https://ucmrwvofyadhnvrsrzpn.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_sWL69mOihUpvLw5LW3EJpA_51FrXk6k',
   APP_TITLE: 'Der Fall Clara Neumann'
 };
-
